6c4023a0e245a817e58f1b9dd539f0e1396c81ed
Karan301205 <karan301205@gmail.com>
2026-03-15 13:02:30 +0530

    corrected firebase var

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8zjuZMJGohwuT4U8c4xKTtZjarqIIf0w",
-  authDomain: "task-management-dashboar-c9983.firebaseapp.com",
-  projectId: "task-management-dashboar-c9983",
-  storageBucket: "task-management-dashboar-c9983.firebasestorage.app",
-  messagingSenderId: "187130481030",
-  appId: "1:187130481030:web:675c92c1c4b0cd22d84407"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
